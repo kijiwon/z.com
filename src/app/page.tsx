5c96c2139d546509f28aedc24f59d4ef9c5ff867
kijiwon <kiji1313@gmail.com>
2024-05-16 22:54:39 +0900

    root page route 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,20 @@
+import Link from "next/link";
+import Image from "next/image";
+import zlog from "../../public/zlogo.png";
+
 export default function Home() {
-  return <div>Z.com</div>;
+  return (
+    <div>
+      <div>
+        <Image src={zlog} alt="logo" />
+      </div>
+      <div>
+        <h1>지금 일어나고 있는 일</h1>
+        <h2>지금 가입하세요</h2>
+        <Link href="/i/flow/signup">계정 만들기</Link>
+      </div>
+      <h3>이미 트위터에 가입하셨나요?</h3>
+      <Link href='/login'>로그인</Link>
+    </div>
+  );
 }
