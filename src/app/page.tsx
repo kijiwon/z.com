547ff8cac8419257257e3acaadcee4aa0432fd19
kijiwon <kiji1313@gmail.com>
2024-05-16 23:15:13 +0900

    css module로 root page 스타일링

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,25 @@
 import Link from "next/link";
 import Image from "next/image";
 import zlog from "../../public/zlogo.png";
+import styles from "../app/page.module.css";
 
 export default function Home() {
   return (
-    <div>
-      <div>
+    <>
+      <div className={styles.left}>
         <Image src={zlog} alt="logo" />
       </div>
-      <div>
+      <div className={styles.right}>
         <h1>지금 일어나고 있는 일</h1>
         <h2>지금 가입하세요</h2>
-        <Link href="/i/flow/signup">계정 만들기</Link>
+        <Link href="/i/flow/signup" className={styles.signup}>
+          계정 만들기
+        </Link>
+        <h3>이미 트위터에 가입하셨나요?</h3>
+        <Link href="/login" className={styles.login}>
+          로그인
+        </Link>
       </div>
-      <h3>이미 트위터에 가입하셨나요?</h3>
-      <Link href='/login'>로그인</Link>
-    </div>
+    </>
   );
 }
