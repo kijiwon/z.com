2b71a922c855c841ddd185a1f2c930a3f6b177a0
kijiwon <kiji1313@gmail.com>
2024-05-18 17:51:25 +0900

    Main 컴포넌트 분리

--- src/app/(beforeLogin)/login/page.tsx
+++ src/app/(beforeLogin)/login/page.tsx
@@ -1,10 +1,7 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import Image from "next/image";
-import Link from "next/link";
-import zLogo from "../../../../public/zlogo.png";
-import styles from "@/app/page.module.css";
+import Main from "../_component/Main";
 
 export default function Login() {
   const router = useRouter();
@@ -12,24 +9,7 @@ export default function Login() {
 
   // replace가 이루어지면 배경이 되는 children은 이 Login 컴포넌트가 됨
   // => Home 컴포넌트와 동일한 컴포넌트를 리턴
-  return (
-    <>
-      <div className={styles.left}>
-        <Image src={zLogo} alt="logo" />
-      </div>
-      <div className={styles.right}>
-        <h1>지금 일어나고 있는 일</h1>
-        <h2>지금 가입하세요</h2>
-        <Link href="/i/flow/signup" className={styles.signup}>
-          계정 만들기
-        </Link>
-        <h3>이미 트위터에 가입하셨나요?</h3>
-        <Link href="/login" className={styles.login}>
-          로그인
-        </Link>
-      </div>
-    </>
-  );
+  return <Main />;
 }
 
 // push vs replace
