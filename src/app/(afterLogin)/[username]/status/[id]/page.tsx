042beaca4f02f8ea831ae45056e0c80b753642a8
kijiwon <kiji1313@gmail.com>
2024-05-21 21:22:32 +0900

    singlePost 컴포넌트 추가

--- src/app/(afterLogin)/[username]/status/[id]/page.tsx
+++ src/app/(afterLogin)/[username]/status/[id]/page.tsx
@@ -1,3 +1,15 @@
-export default function Page() {
-  return <div>개인 페이지</div>;
+import BackButton from "@/app/(afterLogin)/_component/BackButton";
+import style from "./singlePost.module.css";
+import Post from "@/app/(afterLogin)/_component/Post";
+
+export default function SinglePost() {
+  return (
+    <div className={style.main}>
+      <div className={style.header}>
+        <BackButton />
+        <h3 className={style.headerTitle}>게시하기</h3>
+      </div>
+      <Post />
+    </div>
+  );
 }
