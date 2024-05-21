dc8bc50430f63c8b6d58004ff9ca1eddb8050ac5
kijiwon <kiji1313@gmail.com>
2024-05-21 21:36:06 +0900

    commentForm 추가

--- src/app/(afterLogin)/[username]/status/[id]/page.tsx
+++ src/app/(afterLogin)/[username]/status/[id]/page.tsx
@@ -1,6 +1,7 @@
 import BackButton from "@/app/(afterLogin)/_component/BackButton";
 import style from "./singlePost.module.css";
 import Post from "@/app/(afterLogin)/_component/Post";
+import CommentForm from "./_component/CommentForm";
 
 export default function SinglePost() {
   return (
@@ -9,7 +10,13 @@ export default function SinglePost() {
         <BackButton />
         <h3 className={style.headerTitle}>게시하기</h3>
       </div>
-      <Post />
+      <div>
+        <Post />
+      </div>
+      <CommentForm />
+      <div>
+        <Post />
+      </div>
     </div>
   );
 }
