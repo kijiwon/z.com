3896129cea7c39bfd8ff49467e867d2fa3412e70
kijiwon <kiji1313@gmail.com>
2024-05-22 18:40:59 +0900

    더미 데이터로 이미지 추가

--- src/app/(afterLogin)/_component/Post.tsx
+++ src/app/(afterLogin)/_component/Post.tsx
@@ -5,6 +5,7 @@ import relativeTime from "dayjs/plugin/relativeTime"; // -> dayjs의 fromNow를 
 import "dayjs/locale/ko"; // 영어를 한국어로 변환
 import ActionButtons from "./ActionButtons";
 import PostArticle from "./PostArticle";
+import { faker } from "@faker-js/faker";
 
 dayjs.locale("ko");
 dayjs.extend(relativeTime);
@@ -21,6 +22,13 @@ export default function Post() {
     createdAt: new Date(),
     Images: [] as any[],
   };
+
+  // 랜덤하게 이미지 추가하기
+  if (Math.random() > 0.5) {
+    target.Images.push(
+      { imageId: 1, link: faker.image.urlLoremFlickr() } // 랜덤 이미지 생성
+    );
+  }
 
   return (
     <PostArticle post={target}>
@@ -44,7 +52,16 @@ export default function Post() {
             </span>
           </div>
           <div>{target.content}</div>
-          <div className={style.postImageSection}></div>
+          <div className={style.postImageSection}>
+            {target.Images && target.Images.length > 0 && (
+              <Link
+                href={`/${target.User.id}/status/${target.postId}/photo/${target.Images[0].imageId}`}
+                className={style.postImageSection}
+              >
+                <img src={target.Images[0]?.link} alt="" />
+              </Link>
+            )}
+          </div>
           <ActionButtons />
         </div>
       </div>
