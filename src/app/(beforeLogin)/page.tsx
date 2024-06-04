59aacb27e07b267632ca6e2453f93920e844b571
kijiwon <kiji1313@gmail.com>
2024-06-04 14:01:13 +0900

    로그인시에는 '/' 경로 변경

--- src/app/(beforeLogin)/page.tsx
+++ src/app/(beforeLogin)/page.tsx
@@ -1,5 +1,14 @@
+import { auth } from "@/auth";
 import Main from "./_component/Main";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
+  // 로그인한 상태에서 접근시 '/home'으로 리다이렉트
+  if (session?.user) {
+    redirect("/home");
+    return null;
+  }
+
   return <Main />;
 }
