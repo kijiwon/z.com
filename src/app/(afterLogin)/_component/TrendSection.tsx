10a9dae186132c312347e1011a54cf2a130329e8
kijiwon <kiji1313@gmail.com>
2024-06-04 15:21:12 +0900

    login에 따른 레퍼런스 변경

--- src/app/(afterLogin)/_component/TrendSection.tsx
+++ src/app/(afterLogin)/_component/TrendSection.tsx
@@ -3,22 +3,29 @@
 import { usePathname } from "next/navigation";
 import Trend from "./Trend";
 import style from "./trendSection.module.css";
+import { useSession } from "next-auth/react";
 
 export default function TrendSection() {
   const pathname = usePathname();
-
+  const { data } = useSession();
   if (pathname === "/explore") return null;
-
+  if (data?.user) {
+    return (
+      <div className={style.trendBg}>
+        <div className={style.trend}>
+          <Trend />
+          <Trend />
+          <Trend />
+          <Trend />
+          <Trend />
+          <Trend />
+        </div>
+      </div>
+    );
+  }
   return (
     <div className={style.trendBg}>
-      <div className={style.trend}>
-        <Trend />
-        <Trend />
-        <Trend />
-        <Trend />
-        <Trend />
-        <Trend />
-      </div>
+      <div className={style.noTrend}>트렌드를 가져올 수 없습니다.</div>
     </div>
   );
 }
