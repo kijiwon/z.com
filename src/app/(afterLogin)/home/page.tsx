0e47c73af7b060497273da8d3d6b849c66804e88
kijiwon <kiji1313@gmail.com>
2024-06-05 18:45:53 +0900

    home컴포넌트에서 react-query에 접근

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,23 +1,46 @@
+import {
+  HydrationBoundary,
+  QueryClient,
+  dehydrate,
+} from "@tanstack/react-query";
 import Post from "../_component/Post";
 import PostForm from "./_component/PostForm";
 import Tab from "./_component/Tab";
 import TabProvider from "./_component/TabProvider";
 import style from "./home.module.css";
 
-export default function Home() {
-  console.log("--home");
+async function getPostRecommends() {
+  const response = await fetch(`http://localhost:9090/api/postRecommends`, {
+    next: {
+      tags: ["posts", "recommends"],
+    },
+    cache: "no-store",
+  });
+}
+
+export default async function Home() {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ["posts", "recommends"],
+    queryFn: getPostRecommends,
+  });
+
+  const dehydratedState = dehydrate(queryClient);
+
   return (
     <main className={style.main}>
-      <TabProvider>
-        <Tab />
-        <PostForm />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-      </TabProvider>
+      <HydrationBoundary>
+        <TabProvider>
+          <Tab />
+          <PostForm />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+        </TabProvider>
+      </HydrationBoundary>
     </main>
   );
 }
