0b248acf61b45ff763b375f5d00dd0528af6d084
kijiwon <kiji1313@gmail.com>
2024-06-12 20:43:16 +0900

    TrendSection에서 data 요청

--- src/app/(afterLogin)/_component/TrendSection.tsx
+++ src/app/(afterLogin)/_component/TrendSection.tsx
@@ -4,12 +4,21 @@ import { usePathname } from "next/navigation";
 import Trend from "./Trend";
 import style from "./trendSection.module.css";
 import { useSession } from "next-auth/react";
+import { useQuery } from "@tanstack/react-query";
+import { getTrends } from "../_lib/getTrends";
 
 export default function TrendSection() {
+  const { data } = useQuery({
+    queryKey: ["trends"],
+    queryFn: getTrends,
+    staleTime: 60 * 1000,
+    gcTime: 300 * 1000,
+  });
+
   const pathname = usePathname();
-  const { data } = useSession();
+  const { data: session } = useSession(); // 겹치는 변수는 이름을 바꿔줌
   if (pathname === "/explore") return null;
-  if (data?.user) {
+  if (session?.user) {
     return (
       <div className={style.trendBg}>
         <div className={style.trend}>
