95af31dd00d44f536ad23c2bb9031480f2bf2858
kijiwon <kiji1313@gmail.com>
2024-06-13 14:50:04 +0900

    explore 페이지에 TrendSection 생성

--- src/app/(afterLogin)/explore/page.tsx
+++ src/app/(afterLogin)/explore/page.tsx
@@ -1,5 +1,6 @@
 import SearchForm from "../_component/SearchForm";
-import Trend from "../_component/Trend";
+import TrendSection from "./_component/TrendSection";
+
 import style from "./explore.module.css";
 
 export default function Home() {
@@ -10,12 +11,7 @@ export default function Home() {
       </div>
       <div className={style.trend}>
         <h3>나를 위한 트렌드</h3>
-        <Trend />
-        <Trend />
-        <Trend />
-        <Trend />
-        <Trend />
-        <Trend />
+        <TrendSection />
       </div>
     </main>
   );
