7dc7636733b7be2f0408a012068361c1d5576b4b
kijiwon <kiji1313@gmail.com>
2024-06-13 19:23:27 +0900

    Profile 컴포넌트 수정

--- src/app/(afterLogin)/[username]/page.tsx
+++ src/app/(afterLogin)/[username]/page.tsx
@@ -1,12 +1,28 @@
+import {
+  HydrationBoundary,
+  QueryClient,
+  dehydrate,
+} from "@tanstack/react-query";
 import style from "./profile.module.css";
-import Post from "../_component/Post";
 import BackButton from "@/app/(afterLogin)/_component/BackButton";
-import { Post as IPost } from "@/model/Post";
 
 type Props = {
-  post: IPost;
+  params: { username: string };
 };
-export default function Profile() {
+
+export default async function Profile({ params }: Props) {
+  const { username } = params;
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ["users", username],
+    queryFn: getUser,
+  });
+  await queryClient.prefetchQuery({
+    queryKey: ["posts", "users", username],
+    queryFn: getUserPosts,
+  });
+  const dehydratedState = dehydrate(queryClient);
+
   const user = {
     id: "zzionie",
     nickname: "찌오니",
@@ -15,28 +31,30 @@ export default function Profile() {
 
   return (
     <main className={style.main}>
-      <div className={style.header}>
-        <BackButton />
-        <h3 className={style.headerTitle}>{user.nickname}</h3>
-      </div>
-      <div className={style.userZone}>
-        <div className={style.userImage}>
-          <img src={user.image} alt={user.id} />
+      <HydrationBoundary state={dehydratedState}>
+        <div className={style.header}>
+          <BackButton />
+          <h3 className={style.headerTitle}>{user.nickname}</h3>
         </div>
-        <div className={style.userName}>
-          <div>{user.nickname}</div>
-          <div>@{user.id}</div>
+        <div className={style.userZone}>
+          <div className={style.userImage}>
+            <img src={user.image} alt={user.id} />
+          </div>
+          <div className={style.userName}>
+            <div>{user.nickname}</div>
+            <div>@{user.id}</div>
+          </div>
+          <button className={style.followButton}>팔로우</button>
         </div>
-        <button className={style.followButton}>팔로우</button>
-      </div>
-      <div>
-        {/* <Post />
+        <div>
+          {/* <Post />
         <Post />
         <Post />
         <Post />
         <Post />
         <Post /> */}
-      </div>
+        </div>
+      </HydrationBoundary>
     </main>
   );
 }
--- src/app/(afterLogin)/explore/_component/TrendSection.tsx
+++ src/app/(afterLogin)/explore/_component/TrendSection.tsx
@@ -7,7 +7,7 @@ import { getTrends } from "@/app/(afterLogin)/_lib/getTrends";
 
 export default function TrendSection() {
   const { data } = useQuery<Hashtag[]>({
-    queryKey: ["trends"],
+    queryKey: ["trends"], // key가 동일하기 때문에 다른 TrendSection에서 가져온 데이터를 재활용함
     queryFn: getTrends,
     staleTime: 60 * 1000, // fresh -> stale, 5분이라는 기준
     gcTime: 300 * 1000,
