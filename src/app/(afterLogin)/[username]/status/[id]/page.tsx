acdfabbf0929c282c3f9b0b443cd4e2b142c4442
kijiwon <kiji1313@gmail.com>
2024-06-17 16:13:30 +0900

    SinglePost 페이지 적용

--- src/app/(afterLogin)/[username]/status/[id]/page.tsx
+++ src/app/(afterLogin)/[username]/status/[id]/page.tsx
@@ -1,22 +1,24 @@
 import BackButton from "@/app/(afterLogin)/_component/BackButton";
 import style from "./singlePost.module.css";
-import Post from "@/app/(afterLogin)/_component/Post";
 import CommentForm from "./_component/CommentForm";
+import SinglePost from "./_component/SinglePost";
 
-export default function SinglePost() {
+type Props = {
+  params: { id: string };
+};
+
+export default function Page({ params }: Props) {
+  const { id } = params;
+
   return (
     <div className={style.main}>
       <div className={style.header}>
         <BackButton />
         <h3 className={style.headerTitle}>게시하기</h3>
       </div>
-      <div>
-        <Post />
-      </div>
+      <SinglePost id={id} />
       <CommentForm />
-      <div>
-        <Post />
-      </div>
+      <div></div>
     </div>
   );
 }
