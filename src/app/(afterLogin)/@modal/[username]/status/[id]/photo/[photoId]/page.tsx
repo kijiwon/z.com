cbf7e6703740744df1e582226229915a47ee1bc7
kijiwon <kiji1313@gmail.com>
2024-06-17 17:53:56 +0900

    SinglePost 이미지 모달 수정

--- src/app/(afterLogin)/@modal/[username]/status/[id]/photo/[photoId]/page.tsx
+++ src/app/(afterLogin)/@modal/[username]/status/[id]/photo/[photoId]/page.tsx
@@ -5,42 +5,55 @@ import CommentForm from "@/app/(afterLogin)/[username]/status/[id]/_component/Co
 import { faker } from "@faker-js/faker";
 import ActionButtons from "@/app/(afterLogin)/_component/ActionButtons";
 import PhotoModalCloseButton from "./_component/PhotoModalCloseButton";
+import SinglePost from "@/app/(afterLogin)/[username]/status/[id]/_component/SinglePost";
+import Comments from "@/app/(afterLogin)/[username]/status/[id]/_component/Comments";
+import {
+  HydrationBoundary,
+  QueryClient,
+  dehydrate,
+} from "@tanstack/react-query";
+import { getSinglePost } from "@/app/(afterLogin)/[username]/status/[id]/_lib/getSinglePost";
+import { getComments } from "@/app/(afterLogin)/[username]/status/[id]/_lib/getComments";
 
-export default function PhotoModal() {
-  const photo = {
-    imageId: 1,
-    link: faker.image.urlLoremFlickr(),
-    Post: {
-      content: faker.lorem.text(),
-    },
-  };
+type Props = {
+  params: { id: string };
+};
+
+export default async function PhotoModal({ params }: Props) {
+  const { id } = params;
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ["posts", id],
+    queryFn: getSinglePost,
+  });
+  await queryClient.prefetchQuery({
+    queryKey: ["posts", id, "comments"],
+    queryFn: getComments,
+  });
+  const dehydratedState = dehydrate(queryClient);
 
   return (
     <div className={style.container}>
-      <PhotoModalCloseButton />
-      <div className={style.imageZone}>
-        <img src={photo.link} alt={photo.Post?.content} />
-        <div
-          className={style.image}
-          style={{ backgroundImage: `url(${photo.link})` }}
-        />
-        <div className={style.buttonZone}>
-          <div className={style.buttonInner}>
-            <ActionButtons white />
+      <HydrationBoundary state={dehydratedState}>
+        <PhotoModalCloseButton />
+        <div className={style.imageZone}>
+          <img src={photo.link} alt={photo.Post?.content} />
+          <div
+            className={style.image}
+            style={{ backgroundImage: `url(${photo.link})` }}
+          />
+          <div className={style.buttonZone}>
+            <div className={style.buttonInner}>
+              <ActionButtons white />
+            </div>
           </div>
         </div>
-      </div>
-      <div className={style.commentZone}>
-        <Post noImage />
-        <CommentForm />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-      </div>
+        <div className={style.commentZone}>
+          <SinglePost id={id} noImage />
+          <CommentForm />
+          <Comments id={id} />
+        </div>
+      </HydrationBoundary>
     </div>
   );
 }
