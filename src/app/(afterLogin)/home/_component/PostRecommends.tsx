1d68c5667140593d94228e3f327e69de94607978
kijiwon <kiji1313@gmail.com>
2024-06-21 15:29:42 +0900

    무한스크롤링 구현:delay 설정

--- src/app/(afterLogin)/home/_component/PostRecommends.tsx
+++ src/app/(afterLogin)/home/_component/PostRecommends.tsx
@@ -28,7 +28,7 @@ export default function PostRecommends() {
 
   const { ref, inView } = useInView({
     threshold: 0, // 화면에 몇 픽셀이 표시된 이후 이벤트가 발생하는지
-    delay: 0, // 화면에 나타난 이후 몇 초뒤에 이벤트가 발생하는지
+    delay: 1500, // 화면에 나타난 이후 몇 초뒤에 이벤트가 발생하는지
   });
 
   useEffect(() => {
@@ -36,7 +36,7 @@ export default function PostRecommends() {
       // 화면에 보일 떄
       !isFetching && hasNextPage && fetchNextPage(); // 데이터를 가져오는 중이 아닐 떄 다음 페이지가 존재하면 데이터 요청
     }
-  }, [inView, hasNextPage, fetchNextPage, isFetching]);
+  }, [inView, isFetching, hasNextPage, fetchNextPage]);
 
   return (
     <>
