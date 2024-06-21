93d69a6e442827d583d266ebd7de87494e9e9b3e
kijiwon <kiji1313@gmail.com>
2024-06-21 15:19:30 +0900

    무한스크롤링 구현:useInView훅 사용

--- src/app/(afterLogin)/home/_component/PostRecommends.tsx
+++ src/app/(afterLogin)/home/_component/PostRecommends.tsx
@@ -8,10 +8,11 @@ import {
 import { getPostRecommends } from "@/app/(afterLogin)/home/_lib/getPostRecommends";
 import Post from "@/app/(afterLogin)/_component/Post";
 import { Post as IPost } from "@/model/Post";
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
+import { useInView } from "react-intersection-observer";
 
 export default function PostRecommends() {
-  const { data, fetchNextPage, hasNextPage } = useInfiniteQuery<
+  const { data, fetchNextPage, hasNextPage, isFetching } = useInfiniteQuery<
     IPost[],
     Object,
     InfiniteData<IPost[]>,
@@ -25,6 +26,18 @@ export default function PostRecommends() {
     staleTime: 60 * 1000,
   });
 
+  const { ref, inView } = useInView({
+    threshold: 0, // 화면에 몇 픽셀이 표시된 이후 이벤트가 발생하는지
+    delay: 0, // 화면에 나타난 이후 몇 초뒤에 이벤트가 발생하는지
+  });
+
+  useEffect(() => {
+    if (inView) {
+      // 화면에 보일 떄
+      !isFetching && hasNextPage && fetchNextPage(); // 데이터를 가져오는 중이 아닐 떄 다음 페이지가 존재하면 데이터 요청
+    }
+  }, [inView, hasNextPage, fetchNextPage, isFetching]);
+
   return (
     <>
       {data?.pages.map((page, i) => (
@@ -34,7 +47,7 @@ export default function PostRecommends() {
           ))}
         </Fragment>
       ))}
-      <div style={{ height: 50 }} />
+      <div ref={ref} style={{ height: 50 }} />
     </>
   );
 }
