7b603407061cc44aef92452e567a671058015170
kijiwon <kiji1313@gmail.com>
2024-06-28 14:38:35 +0900

    hashtag 검색

--- src/app/(afterLogin)/_component/Trend.tsx
+++ src/app/(afterLogin)/_component/Trend.tsx
@@ -8,7 +8,10 @@ type Props = {
 
 export default function Trend({ trend }: Props) {
   return (
-    <Link href={`/search?q=${trend.title}`} className={style.container}>
+    <Link
+      href={`/search?q=${encodeURIComponent(trend.title)}`} // 주소창의 #제외하기
+      className={style.container}
+    >
       <div className={style.count}>실시간트렌드</div>
       <div className={style.title}>{trend.title}</div>
       <div className={style.count}>{trend.count.toLocaleString()} posts</div>
