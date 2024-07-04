b69f5443ffac98bc662b46505623cedcb850489d
kijiwon <kiji1313@gmail.com>
2024-07-04 20:22:39 +0900

    getUserPosts url 수정

--- src/app/(afterLogin)/[username]/_lib/getUserPosts.ts
+++ src/app/(afterLogin)/[username]/_lib/getUserPosts.ts
@@ -6,12 +6,15 @@ export const getUserPosts: QueryFunction<
   [_1: string, _2: string, string]
 > = async ({ queryKey }) => {
   const [_1, _2, username] = queryKey;
-  const res = await fetch(`http://localhost:9090/api/users/${username}/posts`, {
-    next: {
-      tags: ["posts", "users", username],
-    },
-    cache: "no-store",
-  });
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/users/${username}/posts?cursor=0`,
+    {
+      next: {
+        tags: ["posts", "users", username],
+      },
+      cache: "no-store",
+    }
+  );
 
   if (!res.ok) {
     // This will activate the closest `error.js` Error Boundary
