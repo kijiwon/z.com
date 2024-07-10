7fa0cf0206ab9c2e8fe18f5e554e32ffdbd02d99
kijiwon <kiji1313@gmail.com>
2024-07-10 13:57:17 +0900

    재개시인 경우 컴포넌트 추가

--- src/app/(afterLogin)/_component/Post.tsx
+++ src/app/(afterLogin)/_component/Post.tsx
@@ -19,7 +19,12 @@ type Props = {
 };
 
 export default function Post({ noImage, post }: Props) {
-  const target = post;
+  let target = post;
+
+  if (post.Original) {
+    // 리트윗일 경우 target은 원본 포스트
+    target = post.Original;
+  }
 
   // 링크 클릭시 상위 엘리먼트에 이벤트가 전달되지 않도록하기 -> 상세페이지로 이동하지 않음
   const stopPropagation: MouseEventHandler<HTMLAnchorElement> = (e) => {
@@ -28,6 +33,22 @@ export default function Post({ noImage, post }: Props) {
 
   return (
     <PostArticle post={target}>
+      {/* 리트윗인 경우 */}
+      {post.Original && (
+        <div className={style.postReposted}>
+          <svg
+            viewBox="0 0 24 24"
+            width={16}
+            aria-hidden="true"
+            className="r-14j79pv r-4qtqp9 r-yyyyoo r-10ptun7 r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-1janqcz"
+          >
+            <g>
+              <path d="M4.75 3.79l4.603 4.3-1.706 1.82L6 8.38v7.37c0 .97.784 1.75 1.75 1.75H13V20H7.75c-2.347 0-4.25-1.9-4.25-4.25V8.38L1.853 9.91.147 8.09l4.603-4.3zm11.5 2.71H11V4h5.25c2.347 0 4.25 1.9 4.25 4.25v7.37l1.647-1.53 1.706 1.82-4.603 4.3-4.603-4.3 1.706-1.82L18 15.62V8.25c0-.97-.784-1.75-1.75-1.75z"></path>
+            </g>
+          </svg>
+          재게시했습니다
+        </div>
+      )}
       <div className={style.postWrapper}>
         <div className={style.postUserSection}>
           <Link
