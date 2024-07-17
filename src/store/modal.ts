f1e1ce761f225a8630c9a9a61869b37773831ef6
kijiwon <kiji1313@gmail.com>
2024-07-17 19:32:16 +0900

    modal store 생성

--- src/store/modal.ts
+++ src/store/modal.ts
@@ -1,3 +1,22 @@
 import { create } from "zustand";
 
-export const useModalStore = create(() => {});
+export const useModalStore = create((set) => ({
+  // 초기값
+  mode: "new",
+  data: null,
+  // mode를 바꾸는 set함수
+  setMode(mode) {
+    set({ mode });
+  },
+  // data를 바꾸는 set함수
+  setData(data) {
+    set({ data });
+  },
+  // reset하는 함수
+  reset() {
+    set({
+      mode: "new",
+      data: null,
+    });
+  },
+}));
