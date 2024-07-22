3e311c7130d7d104aea9b1ff23d9ffc44e0fd87e
kijiwon <kiji1313@gmail.com>
2024-07-22 20:29:14 +0900

    revalidate 설정

--- src/app/(afterLogin)/[username]/status/[id]/_lib/getSinglePostServer.ts
+++ src/app/(afterLogin)/[username]/status/[id]/_lib/getSinglePostServer.ts
@@ -1,3 +1,4 @@
+import { revalidateTag } from "next/cache";
 import { cookies } from "next/headers";
 
 export const getSinglePostServer = async ({
@@ -10,12 +11,16 @@ export const getSinglePostServer = async ({
     `${process.env.NEXT_PUBLIC_BASE_URL}/api/posts/${id}`,
     {
       next: {
+        revalidate: 3600, // 3600초 동안 같은 값을 가져옴
         tags: ["posts", id],
       },
       credentials: "include",
       headers: { Cookie: cookies().toString() },
     }
   );
+
+  // posts에 대한 값은 1시간 동안 유지 <- on-demand
+  revalidateTag("posts");
 
   if (!res.ok) {
     throw new Error("Failed to fetch data");
